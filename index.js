d13fae8c62ed30fc8c1425f9704cf09a3efae209
woodb58 <woodb58@gmail.com>
2022-12-05 13:04:49 -0600

    added click and drag functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
+  Matter;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -12,6 +13,11 @@ const render = Render.create({
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
 
 // Walls
 const walls = [
