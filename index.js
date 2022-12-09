935dd63626a62621a193fd873cb1721aec556b54
woodb58 <woodb58@gmail.com>
2022-12-09 16:58:38 -0600

    added color

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 10;
-const cellsVertical = 10;
+const cellsHorizontal = 8;
+const cellsVertical = 6;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
@@ -15,7 +15,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: true,
+    wireframes: false,
     width,
     height,
   },
@@ -133,6 +133,9 @@ horizontals.forEach((row, rowIndex) => {
       {
         label: "wall",
         isStatic: true,
+        render: {
+          fillStyle: "red"
+        }
       }
     );
     World.add(world, wall);
@@ -153,6 +156,9 @@ verticals.forEach((row, rowIndex) => {
       {
         label: "wall",
         isStatic: true,
+        render: {
+          fillStyle: 'red'
+        }
       }
     );
     World.add(world, wall);
@@ -169,6 +175,9 @@ const goal = Bodies.rectangle(
   {
     label: "goal",
     isStatic: true,
+    render: {
+      fillStyle: 'green'
+    }
   }
 );
 World.add(world, goal);
@@ -178,6 +187,9 @@ World.add(world, goal);
 const ballRadius = Math.min(unitLengthX, unitLengthY / 4)
 const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
   label: "ball",
+  render: {
+    fillStyle: 'blue'
+  }
 });
 World.add(world, ball);
 
